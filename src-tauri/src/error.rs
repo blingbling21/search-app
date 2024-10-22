83cc04c2a1f4bc6c7d02bf5d082ddc9a256d795d
zhangliuhuan <zhangliuhuan@chaintor.com>
2024-10-22 17:39:06 +0800

    feat: 完成打开一个透明窗口。

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,25 @@
+use serde::Serialize;
+use thiserror::Error;
+
+
+#[derive(Debug, Error, Serialize)]
+pub enum AppError {
+    #[error("tauri error: {0}")]
+    TauriError(String),
+    #[error("other error {0}")]
+    OtherError(String)
+}
+
+impl From<anyhow::Error> for AppError {
+    fn from(err: anyhow::Error) -> Self {
+        AppError::OtherError(err.to_string())
+    }
+}
+
+impl From<tauri::Error> for AppError {
+    fn from(err: tauri::Error) -> Self {
+        AppError::TauriError(err.to_string())
+    }
+}
+
+pub type AppResult<T> = Result<T, AppError>;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,24 +1,55 @@
-use tauri::generate_handler;
+use std::sync::Arc;
+
+use error::AppResult;
+use log::{error, info};
+use tauri::{generate_handler, AppHandle, WebviewWindowBuilder};
+
+mod error;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .invoke_handler(generate_handler![test])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(generate_handler![test])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn test() {
-  println!("test");
+async fn test(app: AppHandle) -> AppResult<()> {
+    let search_webview_window =
+        WebviewWindowBuilder::new(&app, "search", tauri::WebviewUrl::App("search".into()))
+            .title("search")
+            .center()
+            .always_on_top(true)
+            .transparent(true)
+            .decorations(false)
+            .build()?;
+
+    let search_webview_window_rc = Arc::new(search_webview_window);
+    let search_webview_window_rc_clone = Arc::clone(&search_webview_window_rc);
+
+    search_webview_window_rc.on_window_event(move |we| match we {
+        tauri::WindowEvent::Focused(focus) => {
+            if !focus {
+                if let Ok(_) = search_webview_window_rc_clone.close() {
+                    info!("关闭成功");
+                } else {
+                    error!("关闭失败");
+                }
+            }
+        }
+        _ => info!("其他事件"),
+    });
+
+    Ok(())
 }
