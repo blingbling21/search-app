0bb30c4cc195ce146178bd95863155b3f9c1e771
zhangliuhuan <zhangliuhuan@chaintor.com>
2024-10-24 17:28:11 +0800

    feat: 完成前端搜索页面。

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,79 @@
+"use client";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+import { webviewWindow } from "@tauri-apps/api";
+
 export default function Search() {
-    return <div className=" bg-gray-100 h-20">
-      <input type="text" />
-    </div>
+  // 鼠标进入时取消鼠标穿透
+  const onMouseEnter = async () => {
+    const current = webviewWindow.getCurrentWebviewWindow();
+    await current.setIgnoreCursorEvents(false);
+  };
+  // 鼠标离开时启用鼠标穿透
+  const onMouseLeave = async () => {
+    const current = webviewWindow.getCurrentWebviewWindow();
+    await current.setIgnoreCursorEvents(true);
+  };
+
+  return (
+    <Command
+      className="rounded-lg border shadow-md md:min-w-[450px]"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <Calendar />
+            <span>Calendar</span>
+          </CommandItem>
+          <CommandItem>
+            <Smile />
+            <span>Search Emoji</span>
+          </CommandItem>
+          <CommandItem disabled>
+            <Calculator />
+            <span>Calculator</span>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
 }
